582783137a527c28f253a09ce5fd7de8993da904
SEUNGHO LEE <mfactory86@naver.com>
2024-06-06 02:05:42 +0900

    Add new .json files and solution file for the BoongGod-S project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using BoongGod.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+AuthenticationApi.Setup(app);
+
+app.Run();
---
+++ _/Authentication/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoongGod.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthenticationController : ControllerBase
+{
+	[HttpGet("[action]")]
+	public async Task<string> Register(string id)
+	{
+		await Task.CompletedTask;
+		return id;
+	}
+}
+
+public abstract class AuthenticationApi
+{
+	private const string Controller = "Authentication";
+	
+	public static void Setup(WebApplication app)
+	{
+		app.MapGet($"{Controller}/Login", (string id) =>
+			{
+				return id;
+			})
+			.WithTags(Controller)
+			.WithOpenApi();
+	}
+}
