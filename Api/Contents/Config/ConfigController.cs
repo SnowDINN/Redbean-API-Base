70ae0fe82a331650f53f66178cdb2639837366f7
SEUNGHO LEE <mfactory86@naver.com>
2024-06-10 22:59:30 +0900

    add response model, fixed ConfigController.cs

--- Api/Contents/Config/ConfigController.cs
+++ Api/Contents/Config/ConfigController.cs
@@ -1,26 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Redbean.Extension;
 using Redbean.Firebase;
 
 namespace Redbean.Api.Controllers;
+
+public enum PlatformType
+{
+	Android,
+	iOS
+}
 
 [ApiController]
 [Route("[controller]/[action]")]
 public class ConfigController : ControllerBase
 {
 	[HttpGet]
-	public async Task<string> GetApplicationConfig() => await ConnectConfig("app");
+	public async Task<string> GetApplicationConfig() => await GetConfig("app");
 	
 	[HttpGet]
-	public async Task<string> GetTableConfig() => await ConnectConfig("table");
+	public async Task<string> GetTableConfig() => await GetConfig("table");
+	
+	[HttpPost]
+	public async Task<string> PostAndroidVersion(string version) => await PostVersion(PlatformType.Android, version);
+	
+	[HttpPost]
+	public async Task<string> PostiOSVersion(string version) => await PostVersion(PlatformType.iOS, version);
 
-	private async Task<string> ConnectConfig(string path)
+	private async Task<string> GetConfig(string path)
 	{
 		var document = FirebaseSetting.Firestore.Collection("config").Document(path);
 		var snapshot = await document.GetSnapshotAsync();
 		if (snapshot.Exists)
 		{
-			var user = snapshot.ConvertTo<Dictionary<string, object>>();
-			return ResponseConvert.ToJson(user);
+			var config = snapshot.ConvertTo<Dictionary<string, object>>();
+			return ResponseConvert.ToJson(config);
+		}
+
+		return ResponseConvert.ToJson("Config not found", 1);
+	}
+	
+	private async Task<string> PostVersion(PlatformType type, string version)
+	{
+		var document = FirebaseSetting.Firestore.Collection("config").Document("app");
+		var snapshot = await document.GetSnapshotAsync();
+		if (snapshot.Exists)
+		{
+			var config = snapshot.ToConvert<AppConfigResponse>();
+			var response = new AppVersionResponse();
+			
+			switch (type)
+			{
+				case PlatformType.Android:
+					response.BeforeVersion = config.Android.Version;
+					response.AfterVersion = version;
+					
+					config.Android.Version = version;
+					break;
+				
+				case PlatformType.iOS:
+					response.BeforeVersion = config.iOS.Version;
+					response.AfterVersion = version;
+					
+					config.iOS.Version = version;
+					break;
+			}
+			
+			await document.SetAsync(config.ToDocument());
+			
+			return ResponseConvert.ToJson(response);
 		}
 
 		return ResponseConvert.ToJson("Config not found", 1);
---
+++ Extension/Extension.cs
@@ -0,0 +1,39 @@
+﻿using Google.Cloud.Firestore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Redbean.Extension;
+
+public static class Extension
+{
+	public static T ToConvert<T>(this DocumentSnapshot snapshot) => 
+		JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(snapshot.ToDictionary()));
+
+	public static object ToDocument<T>(this T value) =>
+		JObjectToDictionary(JObject.Parse(JsonConvert.SerializeObject(value)));
+	
+	private static Dictionary<string, object> JObjectToDictionary(JObject obj)
+	{
+		var result = new Dictionary<string, object>();
+
+		foreach (var property in obj.Properties())
+			result[property.Name] = JTokenToObject(property.Value);
+
+		return result;
+	}
+
+	private static object JTokenToObject(JToken token)
+	{
+		switch (token.Type)
+		{
+			case JTokenType.Object:
+				return JObjectToDictionary(token as JObject);
+        
+			case JTokenType.Array:
+				return (from item in (JArray)token select JTokenToObject(item)).ToList();;
+        
+			default:
+				return (token as JValue).Value;
+		}
+	}
+}
