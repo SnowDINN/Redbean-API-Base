256999c013b517e4a98797d881b765b5feb0b1c4
SEUNGHO LEE <mfactory86@naver.com>
2024-06-21 21:49:10 +0900

    add redis server

--- Scripts/Libraries/Bootstrap/FirebaseBootstrap.cs
+++ Scripts/Libraries/Bootstrap/FirebaseBootstrap.cs
@@ -11,7 +11,7 @@ public class FirebaseBootstrap : IBootstrap
 
 	public async Task Setup()
 	{
-		var environmentPath = $@"{Directory.GetCurrentDirectory()}\Environment\Firebase\DEV.json";
+		var environmentPath = $@"{Directory.GetCurrentDirectory()}\Properties\Firebase\DEV.json";
 		Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", environmentPath);
 
 		using var reader = new StreamReader(environmentPath);
